d5371a477351b4ca2e86a218c92e819cf238f091
Tim K <timprogrammer@rambler.ru>
2019-11-13 17:42:45 +0300

    Imported TinyExpr source code and added a distclean script

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 rm -vf libyatc.a *.o yatc
 export CFLAGS="-g -std=c99 -Wall -Wextra $*"
-for file in cext.c vmcommon.c vmio.c vmcore.c
+for file in cext.c vmcommon.c vmio.c vmcore.c tinyexpr/tinyexpr.c
 do
 	printf '\t%s\t%s\n' cc $file
-	cc -c $CFLAGS -I. -o `basename $file .c`.o $file || exit 3
+	cc -c $CFLAGS -I. -Itinyexpr -o `basename $file .c`.o $file || exit 3
 done
 ar crs libyatc.a *.o || exit 1
 for testn in test01-splitstring test02-upperlowerreverse test03-variablecontext
 do
 	rm -f tests/$testn.bin
 	printf '\t%s\t%s\n' ld $testn
-	cc $CFLAGS -I. -o tests/$testn.bin tests/$testn.c libyatc.a || exit 2
+	cc $CFLAGS -I. -o tests/$testn.bin tests/$testn.c libyatc.a -lm || exit 2
 done
 printf '\t%s\t%s\n' ld yatc
-cc $CFLAGS -I. -o yatc main.c libyatc.a || exit 4
+cc $CFLAGS -I. -o yatc main.c libyatc.a -lm || exit 4
 exit 0
---
+++ distclean.sh
@@ -0,0 +1,2 @@
+#!/bin/sh -e
+rm -vfr *.o tinyexpr/*.o tests/*.bin yatc libyatc.a
